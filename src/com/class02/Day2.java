3c95f32728a729ac172be5a25370930a682d96a6
onureker502 <onureker@Onurs-MacBook-Air.local>
2020-03-17 21:13:10 -0400

    rainy

--- src/com/class02/Day2.java
+++ src/com/class02/Day2.java
@@ -5,6 +5,12 @@ public class Day2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		boolean isRainig=true;
+		if (isRainig) {
+			System.out.println("rainy");
+			
+		}
 	}
 
 }
